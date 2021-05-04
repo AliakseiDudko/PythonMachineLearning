31ea8561d6aa76577231d42e1f33ce96f02946d1
Aliaksei Dudko <aliaksei.dudko@kaltura.com>
2021-05-04 13:42:54 +0300

    Implement k-nearest neighbor algorithm

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+import numpy
+import pandas
+import sklearn.neighbors
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Set random seed to get stable results for debugging
+numpy.random.seed(5)
 
+# Fill missing Embarked with most frequent value
+tbl = pandas.read_csv("data.csv")
+tbl["Embarked"] = tbl["Embarked"].fillna(tbl["Embarked"].value_counts().index[0])
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Fill missing Fare based on mean ticket price for each Pclass
+pclassFareMean = tbl[tbl["Fare"] > 0].groupby(["Pclass"])["Fare"].mean()
+for pclass, fare in pclassFareMean.iteritems():
+    tbl.loc[(tbl["Pclass"] == pclass) & (tbl["Fare"].isnull()), "Fare"] = fare
 
+# Add Title column
+tbl["Title"] = tbl["Name"].str.extract("([A-Za-z]+)\.", expand=True)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Fill missing Age based on mean age for each title
+titleAgeMean = tbl[tbl["Age"] > 0].groupby("Title")["Age"].mean()
+for title, age in titleAgeMean.iteritems():
+    tbl.loc[(tbl["Title"] == title) & (tbl["Age"].isnull()), "Age"] = age
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Add new feature IsAlone, HasCabin
+tbl["IsAlone"] = tbl["SibSp"] + tbl["Parch"] == 0
+tbl["HasCabin"] = tbl["Cabin"].isnull() != True
+
+# print(tbl.to_string())
+
+# Build dummy columns
+tbl = pandas.get_dummies(tbl, columns=["Sex", "Pclass", "Embarked", "Title"], drop_first=False)
+
+# Drop extra columns
+tbl = tbl.drop(["PassengerId", "Name", "Ticket", "Cabin"], axis=1)
+
+# Normalize columns
+for column in ["Age", "SibSp", "Parch", "Fare"]:
+    tbl[column] = ((tbl[column] - tbl[column].min()) / (tbl[column].max() - tbl[column].min())) * 1
+
+# print(tbl.to_string())
+# print(tbl.corr().to_string())
+
+# KNN algorithm
+X = tbl.drop(["Survived"], axis=1)
+Y = tbl[["Survived"]]
+
+X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(X, Y)
+
+alg = sklearn.neighbors.NearestCentroid()
+alg.fit(X_train, Y_train.values.ravel())
+
+score_train = alg.score(X_train, Y_train)
+score_test = alg.score(X_test, Y_test)
+print("-------------------------------------------------------")
+print(f"KNN Train: {score_train},  Test: {score_test}")
